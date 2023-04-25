e1c6878a19a24eca0932f0878347daf3b671c34c
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-04-25 19:01:24 +0200

    pushing_visualisation

---
+++ Help_protect_great_barrier_reef/visualisation/visualisation.py
@@ -0,0 +1,48 @@
+import pandas as pd 
+import ast
+import os
+from PIL import Image, ImageDraw
+
+df_train=pd.read_csv("train.csv")
+
+def visualise(path: str)->Image:
+    """
+    The goal of this function
+    is the visualisation of the
+    image with annotated position 
+    of the starfish position if 
+    there is any
+    
+    Arguments:
+        -path: The path to the 
+        image to display
+        
+    Returns:
+        -Image: The visualisation
+        desired
+    """
+    
+    if not os.path.exists(path):
+        raise AssertionError("The path you entered does not exist, \
+                             check all existing path by going into train_images file")
+    
+    image = Image.open(path)
+
+    splitted_path=path.split("/")
+    video_number, image_number = splitted_path[-2].replace("video_",""),\
+          splitted_path[-1].replace(".jpg","")
+    image_id=video_number+"-"+image_number
+    annotation=df_train[df_train.image_id==image_id]["annotations"].values[0]
+    
+    if annotation=="[]":
+        pass
+    else:
+        image_annotation=ast.literal_eval(annotation)[0]
+        x0, y0, x1, y1 = image_annotation["x"], image_annotation["y"],\
+         image_annotation["x"]+image_annotation["width"],\
+              image_annotation["y"]+image_annotation["height"]
+        draw = ImageDraw.Draw(image)
+        draw.rectangle((x0, y0, x1, y1), outline="red", width=3)
+
+    image.show()
+
