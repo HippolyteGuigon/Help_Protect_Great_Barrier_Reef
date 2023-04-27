e210f50eb6188783a43f77e933f2065844f2267b
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-04-27 15:21:44 +0200

    pushing_test

--- Help_protect_great_barrier_reef/model/model.py
+++ Help_protect_great_barrier_reef/model/model.py
@@ -12,6 +12,11 @@ from tqdm import tqdm
 from tqdm.contrib import tzip
 from Help_protect_great_barrier_reef.preprocessing.preprocessing import preprocessing_yolo, clean_all_files
 from Help_protect_great_barrier_reef.logs.logs import main
+from Help_protect_great_barrier_reef.configs.confs import load_conf, clean_params, Loader
+
+main_params = load_conf("configs/main.yml", include=True)
+main_params = clean_params(main_params)
+split_path = main_params["split_path"]
 
 main()
 
@@ -54,23 +59,24 @@ class yolo_model:
                                  please call the get_split method")
         
         for path_set in ["train_set", "test_set", "valid_set"]:
-            if not os.path.exists(path_set):
-                os.mkdir(path_set)
+            full_path=os.path.join(split_path,path_set)
+            if not os.path.exists(full_path):
+                os.mkdir(full_path)
             else:
-                shutil.rmtree(path_set)
-                os.mkdir(path_set)     
+                shutil.rmtree(full_path)
+                os.mkdir(full_path)     
 
         logging.info("Splitting the files between the different sets...")
 
         for train_image_path, test_image_path, valid_image_path\
               in tzip(self.train_images, self.test_images, self.val_images):
-            shutil.copy(train_image_path, "train_set")
-            shutil.copy(test_image_path, "test_set")
-            shutil.copy(valid_image_path, "valid_set")
+            shutil.copy(train_image_path, os.path.join(split_path,"train_set"))
+            shutil.copy(test_image_path, os.path.join(split_path,"test_set"))
+            shutil.copy(valid_image_path, os.path.join(split_path,"valid_set"))
 
-            shutil.copy(train_image_path.replace(".jpg", ".txt"), "train_set")
-            shutil.copy(test_image_path.replace(".jpg", ".txt"), "test_set")
-            shutil.copy(valid_image_path.replace(".jpg", ".txt"), "valid_set")
+            shutil.copy(train_image_path.replace(".jpg", ".txt"),  os.path.join(split_path,"train_set"))
+            shutil.copy(test_image_path.replace(".jpg", ".txt"),  os.path.join(split_path,"train_set"))
+            shutil.copy(valid_image_path.replace(".jpg", ".txt"),  os.path.join(split_path,"train_set"))
 
 if __name__ == '__main__':
     test=yolo_model()
