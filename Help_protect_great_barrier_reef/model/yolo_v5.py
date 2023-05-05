4f54ebf1d70baf47f0a4b56110c0b7d57c8c8597
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-05-05 15:39:51 +0200

    pushing_visualisation

--- Help_protect_great_barrier_reef/model/yolo_v5.py
+++ Help_protect_great_barrier_reef/model/yolo_v5.py
@@ -24,6 +24,8 @@ fitted_model_path = main_params["fitted_model_path"]
 class yolo_model:
 
     def __init__(self):
+        print("ICIIIIIII", os.getcwd())
+        print("ICIIIIIII", os.listdir())
         self.df=pd.read_csv("train.csv")
         preprocess=preprocessing_yolo(self.df)
         preprocess.full_conversion()
