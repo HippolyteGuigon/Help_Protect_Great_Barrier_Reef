3f5665d27311c04e6125ea2ab2805f26bbf61180
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-05-07 15:22:42 +0200

    pushing_files_allocation_MaskRCNN

--- Help_protect_great_barrier_reef/model/MaskRCNN.py
+++ Help_protect_great_barrier_reef/model/MaskRCNN.py
@@ -3,10 +3,15 @@
 import glob
 import shutil
 import numpy as np
+import logging
+import os
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 from Help_protect_great_barrier_reef.logs.logs import main
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
 
 class MaskRCNN:
     """
@@ -16,7 +21,7 @@ class MaskRCNN:
     """
 
     def __init__(self)->None:
-        pass
+        logging.warning("MASK RCNN Model launched !")
 
     def split_images(self, train_size:float=0.9)->None:
         """
@@ -33,13 +38,35 @@ class MaskRCNN:
             -None
         """
 
+        logging.info("Allocating images in train/val set...")
+
+        train_image_path="Help_protect_great_barrier_reef/model/Custom_MaskRCNN/samples/custom/dataset/train"
+        val_image_path="Help_protect_great_barrier_reef/model/Custom_MaskRCNN/samples/custom/dataset/val"
+
+        for file in os.listdir(train_image_path):
+            os.remove(os.path.join(train_image_path,file))
+        for file in os.listdir(val_image_path):
+            os.remove(os.path.join(val_image_path,file))
+
         all_images=glob.glob("train_images/*/*.jpg")
 
-        train_set=np.random.choice(all_images, 
-                                   size=np.floor(0.9*len(all_images)), replace=False)
-        
-        val_set=[image_path for image_path in all_images if image_path not in train_set]
+        train_set, val_set = train_test_split(all_images, test_size=0.1)
 
+        for image_train_path in tqdm(train_set):
+            shutil.copy(image_train_path,train_image_path)
+            entire_name=image_train_path.split("/")[-2]+"_"+image_train_path.split("/")[-1]
+            os.rename(os.path.join(train_image_path,
+                                   image_train_path.split("/")[-1]), os.path.join(
+                train_image_path,entire_name))
+
+        for image_val_path in tqdm(val_set):
+            shutil.copy(image_val_path,val_image_path)
+            entire_name=image_val_path.split("/")[-2]+"_"+image_val_path.split("/")[-1]
+            os.rename(os.path.join(val_image_path,
+                                   image_val_path.split("/")[-1]), os.path.join(
+                val_image_path,entire_name))
+            
+        logging.info("Images allocated !")
 
 if __name__ == '__main__':
     main()
