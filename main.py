0fe21e4e2120607d01e5b5176eacaa6a53011029
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-05-14 12:18:16 +0200

    pushing_new_gitignore

--- main.py
+++ main.py
@@ -1,45 +1,59 @@
-#Ici, on veut lancer l'intégralité du Pipeline, 
-#C'est à dire à la fois le fit ainsi que le predict
-
 import os
 import argparse
 import logging
 import torch
-from Help_protect_great_barrier_reef.model.yolo_v5 import yolo_model, get_last_model_path
-from Help_protect_great_barrier_reef.configs.confs import load_conf, clean_params, Loader
+from Help_protect_great_barrier_reef.model.yolo_v5 import (
+    yolo_model,
+    get_last_model_path,
+)
+from Help_protect_great_barrier_reef.configs.confs import (
+    load_conf,
+    clean_params,
+    Loader,
+)
 from Help_protect_great_barrier_reef.logs.logs import main
 
 main_params = load_conf("configs/main.yml", include=True)
 main_params = clean_params(main_params)
-all_fitted_model_path=main_params["all_fitted_model_path"]
+all_fitted_model_path = main_params["all_fitted_model_path"]
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--model", type=str, default="Yolo", help="Model chosen")
-parser.add_argument("--fitting", type=str,choices=['fit', 'load'], default="fit", help="Wheter the model should\
-                     be trained or loaded")
-parser.add_argument("--train_size", type=float, default=0.8, help="Size of the train\
-                    set for the model fitting")
+parser.add_argument(
+    "--fitting",
+    type=str,
+    choices=["fit", "load"],
+    default="fit",
+    help="Wheter the model should\
+                     be trained or loaded",
+)
+parser.add_argument(
+    "--train_size",
+    type=float,
+    default=0.8,
+    help="Size of the train\
+                    set for the model fitting",
+)
 
-args=parser.parse_args()
+args = parser.parse_args()
 
-if args.model=="Yolo":
+if args.model == "Yolo":
     logging.info("You have chosen the Yolo model")
-    model=yolo_model()
+    model = yolo_model()
 
-if args.fitting=="fit":
+if args.fitting == "fit":
     logging.info("You have chosen to fit the model")
     model.get_split(train_size=args.train_size)
     model.split_files()
     model.fit()
+    model.model_loading()
 
-elif args.fitting=="load":
+elif args.fitting == "load":
     logging.info("You have chosen to load the model")
-    
-    last_model_path=get_last_model_path()
 
-    model = torch.hub.load(
-                "ultralytics/yolov5", "custom", last_model_path
-                )
-    
+    last_model_path = get_last_model_path()
+
+    model = torch.hub.load("ultralytics/yolov5", "custom", last_model_path)
+
     logging.info("Model successfully loaded !")
