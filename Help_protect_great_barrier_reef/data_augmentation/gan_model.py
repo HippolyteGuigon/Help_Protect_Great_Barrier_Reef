bc1547d404df02f9c3a07ad32de5a92f15f090b9
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-05-21 10:26:03 +0200

    pushing_gan_functions

--- Help_protect_great_barrier_reef/data_augmentation/gan_model.py
+++ Help_protect_great_barrier_reef/data_augmentation/gan_model.py
@@ -6,7 +6,7 @@ import ast
 from tqdm import tqdm
 from PIL import Image
 from math import sqrt
-from keras.optimizers import Adam
+from keras.optimizers import Adam, SGD, Adamax
 from keras.initializers import RandomNormal
 from numpy import zeros, ones, expand_dims, asarray
 from numpy.random import randn, randint
@@ -24,7 +24,8 @@ class GAN_model:
         self.X_train = []
         self.df = pd.read_csv("train.csv")
 
-    def convert_images(self, only_starfish: bool = True) -> None:
+    def convert_images(self, only_starfish: bool = True,
+                    reload: bool = False) -> None:
         """
         The goal of this function
         is converting all images to
@@ -35,7 +36,8 @@ class GAN_model:
             -only_starfish: bool: Whether
             or not only images with starfish
             should be considered
-
+            -reload: bool: Whether images should
+            be reloaded in the folder
         Returns:
             -None
         """
@@ -55,9 +57,10 @@ class GAN_model:
         else:
             images_path = glob.glob("train_images/*/*.jpg")
 
-        if not os.path.exists("X_train.npy"):
+        if not os.path.exists("X_train.npy") or reload:
             for image in tqdm(images_path):
                 img = Image.open(image)
+                img = img.resize((200, 200))
                 numpydata = asarray(img)
                 self.X_train.append(numpydata)
             self.X_train = np.array(self.X_train)
@@ -90,11 +93,14 @@ class GAN_model:
             pyplot.subplot(10, 10, 1 + i)
             pyplot.axis("off")
             pyplot.imshow(X[i, :, :, 0], cmap="gray_r")
+        last_model=glob.glob("*.h5")
+        if len(last_model)>0:
+            os.remove(last_model[0])
         filename2 = "model_%04d.h5" % (step + 1)
         g_model.save(filename2)
         print(">Saved: %s" % (filename2))
 
-    def define_discriminator(self, in_shape=(720, 1280, 3)):
+    def define_discriminator(self, in_shape=(200, 200, 3)):
         init = RandomNormal(stddev=0.02)
         in_image = Input(shape=in_shape)
         fe = Flatten()(in_image)
@@ -109,7 +115,7 @@ class GAN_model:
         fe = Dropout(0.3)(fe)
         out = Dense(1, activation="sigmoid")(fe)
         model = Model(in_image, out)
-        opt = Adam(lr=0.0002, beta_1=0.5)
+        opt = Adamax(lr=0.02)
         model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
         return model
 
@@ -122,9 +128,9 @@ class GAN_model:
         gen = LeakyReLU(alpha=0.2)(gen)
         gen = Dense(1024, kernel_initializer=init)(gen)
         gen = LeakyReLU(alpha=0.2)(gen)
-        gen = Dense(28 * 28 * 1, kernel_initializer=init)(gen)
+        gen = Dense(200 *200 * 3, kernel_initializer=init)(gen)
         out_layer = Activation("tanh")(gen)
-        out_layer = Reshape((28, 28, 1))(gen)
+        out_layer = Reshape((200, 200, 3))(gen)
         model = Model(in_lat, out_layer)
         return model
 
@@ -132,12 +138,12 @@ class GAN_model:
         d_model.trainable = False
         gan_output = d_model(g_model.output)
         model = Model(g_model.input, gan_output)
-        opt = Adam(lr=0.0002, beta_1=0.5)
+        opt = Adamax(lr=0.02)
         model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
         return model
 
     def train(
-        self, g_model, d_model, gan_model, X_train, latent_dim, n_epochs=100, n_batch=64
+        self, g_model, d_model, gan_model, X_train, latent_dim, n_epochs=10, n_batch=64
     ):
         bat_per_epo = int(X_train.shape[0] / n_batch)
         n_steps = bat_per_epo * n_epochs
@@ -156,10 +162,16 @@ class GAN_model:
             if (i + 1) % (bat_per_epo * 1) == 0:
                 self.summarize_performance(i, g_model, latent_dim)
 
+    def save_plot(self, examples, n_examples):
+        for i in range(n_examples):
+            pyplot.subplot(sqrt(n_examples), sqrt(n_examples), 1 + i)
+            pyplot.axis('off')
+            pyplot.imshow(examples[i, :, :, 0], cmap='gray_r')
+        pyplot.show()
 
 if __name__ == "__main__":
     test = GAN_model()
-    test.convert_images()
+    test.convert_images(only_starfish=False)
     latent_dim = 100
     discriminator = test.define_discriminator()
     generator = test.define_generator(100)
@@ -170,6 +182,14 @@ if __name__ == "__main__":
         gan_model,
         test.X_train,
         latent_dim,
-        n_epochs=20,
-        n_batch=64,
+        n_epochs=50,
+        n_batch=1000,
     )
+    last_model=glob.glob("*.h5")[0]
+    model = load_model(last_model)
+    latent_dim=100
+    n_examples=9
+    latent_points=test.generate_latent_points(latent_dim,n_examples)
+    X=model.predict(latent_points)
+    X=(X+1)/2
+    test.save_plot(X, n_examples)
